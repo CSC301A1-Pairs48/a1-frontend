cf3847c612c47454d92ff090ebad558697861e7d
TomKan <tomkan0909@gmail.com>
2022-02-09 17:37:54 -0500

    changed getstaticprops url

--- pages/shop.js
+++ pages/shop.js
@@ -92,8 +92,7 @@ export async function getStaticProps() {
 
 	// TODO : change the url here so that if fetches based on prod or test env
 	// const res = await fetch('http://localhost:5000/punks');
-	const res = await fetch('https://floating-peak-57781.herokuapp.com/punks');
-	// console.log(res);
+	const res = await fetch('https://a1-server-tomkan0909-a1-48.vercel.app/api/punks');
 	const shopItems = await res.json();
 
 	return {
